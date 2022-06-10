9e11cab9c250eecba4acdef2fe2279cb8c7ca73d
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-06-10 10:31:23 -0300

    feat: reorder registers

--- packages/common/src/registers.ts
+++ packages/common/src/registers.ts
@@ -1,12 +1,11 @@
 export function encodeRegister(register: string): number {
   return {
-    A: 0b000,
-    B: 0b001,
-    IP: 0b010,
-    SP: 0b011,
-    BP: 0b100,
-    F: 0b101,
-    AB: 0b110,
+    A: 0b0000,
+    B: 0b0001,
+    C: 0b0010,
+    F: 0b0011,
+    SP: 0b100,
+    BP: 0b101,
   }[register];
 }
 
@@ -14,19 +13,18 @@ export function decodeRegister(registerCode: number): string {
   return {
     0b000: "A",
     0b001: "B",
-    0b010: "IP",
-    0b011: "SP",
-    0b100: "BP",
-    0b101: "F",
-    0b110: "AB",
+    0b010: "C",
+    0b011: "F",
+    0b100: "SP",
+    0b101: "BP",
   }[registerCode];
 }
 
 export const RegistersColors = {
   A: "blue.600",
   B: "green.600",
+  C: "red.600",
   IP: "purple.600",
   SP: "orange.600",
   BP: "pink.600",
-  AB: "gray.600",
 };
