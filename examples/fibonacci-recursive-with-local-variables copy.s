71fe0e4b3bcf29422418d06e7cf29d17680f954a
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-06-10 14:00:27 -0300

    feat: fix stack

--- examples/fibonacci-recursive-with-local-variables copy.s
+++ examples/fibonacci-recursive-with-local-variables copy.s
@@ -6,7 +6,7 @@ start:
 
 fib:
     SUB SP, 1       ; Allocate space for local variable
-    mov a, [BP+2]   ; a = n
+    mov a, [BP+3]   ; a = n
     cmp a, 1        ; if a < 1
     jlez fib_end    ; then return 1
     sub a, 1        ; a = n - 1
@@ -14,14 +14,14 @@ fib:
     call fib        ; a = fib(n - 1)
     mov SP, BP      ; clear parameter stack
     SUB SP, 1       ; restore local variable
-    mov [BP-1], a   ; save result in local variable
-    mov a, [BP+2]   ; a = n
+    mov [BP], a   ; save result in local variable
+    mov a, [BP+3]   ; a = n
     sub a, 2        ; a = n - 2
     push a          ; push n - 2
     call fib        ; a = fib(n - 2)
     mov SP, BP      ; clear parameter stack, but fib(n - 1) is on top of stack
     SUB SP, 1       ; restore local variable
-    mov b, [BP-1]   ; b = saved result
+    mov b, [BP]   ; b = saved result
     add a, b        ; a = fib(n - 2) + fib(n - 1)
     ADD SP, 1       ; free local variable
     ret
