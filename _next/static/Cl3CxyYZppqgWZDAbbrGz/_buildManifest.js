520ce49e7f78d9fafda694d44932e1110f519c8f
wilgnne <wilgnne.kba@gmail.com>
2022-06-19 02:13:56 +0000

    Deploying to gh-pages from  @ ba0eac1539a6efa88ab028a1b429e90a6d6996d0 🚀

---
+++ _next/static/Cl3CxyYZppqgWZDAbbrGz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/984-6d84b88864967602.js","static/chunks/pages/index-f1b1164e17900272.js"],"/_error":["static/chunks/pages/_error-5f79c00932c7c9f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
